3b648cd01605cbdddbd47183d189b1b4a82c04ae
TedKweeBintoro <tehsauscabe@DESKTOP-1S4OG66.localdomain>
2021-09-13 00:07:09 -0500

    added all exec pics, new ballot buzz

--- src/pages/execs.js
+++ src/pages/execs.js
@@ -32,7 +32,7 @@ const SciMUN = () => (
         alignItems: 'center',
         justifyContent: 'center',
       }}>
-        <p><i>PRESIDENT: </i><b>Ted Kwee-Bintoro</b></p>
+        <p>PRESIDENT: <b>Ted Kwee-Bintoro</b></p>
       </Col>
     </Row>
     <Row>
@@ -50,16 +50,146 @@ const SciMUN = () => (
         </ul>
       </Col>
     </Row>
+    <hr></hr>
     <Row>
-      <Col>
-        <p>Director of Competitions: Audrey Hopkin</p>
-        <p>Director of Events: Daphne Sahaya</p>
+      <Col style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <StaticImage src="../images/audrey-headshot.jpg" placeholder={ "blurred" } height={ 200 } quality={ 100 } layout={ "constrained" }/>
+      </Col>
+      <Col style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <StaticImage src="../images/daphne-headshot.jpg" placeholder={ "blurred" } height={ 200 } quality={ 100 } layout={ "constrained" }/>
       </Col>
     </Row>
     <Row>
-      <Col>
-        <p>Director of Logistics: Yifan Guo</p>
-        <p>Director of Outreach: Anay Gupta</p>
+    <Col style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <p>DIRECTOR OF COMPETITIONS: <b>Audrey Hopkin</b></p>
+      </Col>
+      <Col style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <p>DIRECTOR OF EVENTS: <b>Daphne Sahaya</b></p>
+      </Col>
+    </Row>
+    <Row>
+      <Col style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <ul>
+          <li>has a titanium spine</li>
+          <li>thinks the moon should be a planet</li>
+          <li>knows the first 43 digits of pi (don't ask what #42 and #43 are)</li>
+        </ul>
+      </Col>
+      <Col style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <ul>
+          <li>reps traditional science</li>
+          <li>is 6' 4"</li>
+          <li>loves all the committee heads equally (more or less)</li>
+        </ul>
+      </Col>
+    </Row>
+    <hr></hr>
+    <Row>
+      <Col style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <StaticImage src="../images/anay-headshot.jpg" placeholder={ "blurred" } height={ 200 } quality={ 100 } layout={ "constrained" }/>
+      </Col>
+      <Col style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        }}>
+          <StaticImage src="../images/yifan-headshot.jpg" placeholder={ "blurred" } height={ 200 } quality={ 100 } layout={ "constrained" }/>
+      </Col>
+    </Row>
+    <Row>
+      <Col style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <p>DIRECTOR OF OUTREACH: <b>Anay Gupta</b></p>
+      </Col>
+      <Col style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <p>DIRECTOR OF LOGISTICS: <b>Yifan Guo</b></p>
+      </Col>
+    </Row>
+    <Row>
+      <Col style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <ul>
+          <li>lost his id like three times idk</li>
+          <li>speaks fluent chinese</li>
+          <li>jai hind</li>
+        </ul>
+      </Col>
+      <Col style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <ul>
+          <li>can do pushups with one hand</li>
+          <li>lived in three countries</li>
+          <li>loves eating</li>
+        </ul>
       </Col>
     </Row>
     <Link to="/">Go back to the homepage</Link>
